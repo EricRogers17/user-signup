d12046cf88769e126d2b25cc3d60145aee60325b
Eric Rogers <eric.rog17@gmail.com>
2017-07-05 20:11:51 -0700

    Finished user-signup project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, request, render_template, flash
-
+from flask import Flask, redirect, request, render_template
+import cgi
+import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -17,25 +18,51 @@ def display_signup_page():
     return render_template('signup.html')
 
 
-@app.route('/welcome', methods=['POST', 'GET'])
+@app.route('/signup', methods=['POST'])
 def validate_signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-    return render_template('welcome.html')
-    # username and password musn't have spaces and
-    # and be between 3-20 characters
+        email = request.form['email']
+        # error variables
+        username_error = ''
+        password_error = ''
+        verify_error = ''
+        email_error = ''
+    # username and password musn't have spaces and be between 3-20 characters
+    if len(username) < 3 or len(username) > 20:
+        username_error = 'Username must be between 3-20 charaters'
+
+    if ' ' in username:
+        username_error = 'Username cannot contain spaces'
+
+    if len(password) < 3 or len(password) > 20:
+        password_error = 'Password must be between 3-20 charaters'
+
+    if ' ' in password:
+        password_error = 'Password cannot contain spaces'
 
     # user's password and verify don't match
-    # if not password == verify:
-    # flash("Verify Password does not match Password.")
-    # return redirect('/signup')
-    # else:
-    # pass
-    # user provided a non-valid email. valid email has
-    # one @, a single ., no spaces, and is between 3-20 characters
+    if password != verify:
+        verify_error = "Password and Verify Password don't match"
 
+    # email has one @, a single ., no spaces, and between 3-20 characters
+    if email == '':
+        pass
+    if ' ' in email:
+        email_error = "Email can't have spaces"
+    else:
+        if '@' not in email and '.' not in email:
+            email_error = "Your email must contain one @ symbol and one '.'"
+
+    if len(email) < 3 or len(email) > 20:
+        email_error = "Email must have one @, a single ., no spaces, and be between 3-20 character"
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', username=username)
+    else:
+        return render_template('signup.html', name=username, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
 
 if __name__ == '__main__':
     app.run()
